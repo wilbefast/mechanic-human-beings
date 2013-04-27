b368fc48d9174e58dba83844246fa5132ad8c773
wilbefast <wilbefast@gmail.com>
2013-04-27 14:07:26 +0200

    vibrating 'bubbles'

--- love_prototype/game.lua
+++ love_prototype/game.lua
@@ -24,6 +24,52 @@ local Class = require("hump/class")
 TARGETS
 --]]------------------------------------------------------------
 
+local Target = Class
+{
+  init = function(self)
+    self.x = math.random()*love.graphics.getWidth()
+    self.y = 0
+    self.heartrate = math.random()
+    self.wobble = 0
+  end,
+      
+  INTERVAL = 10,
+  
+  SPEED = 16,
+  
+  SIZE = 32,
+  
+  DAMAGE_THRESHOLD = 0.2
+}
+
+function Target:draw()
+  
+  local circle_size = self.SIZE * (1 + 0.3*useful.signedRand(self.wobble))
+  
+  love.graphics.push()
+    love.graphics.setColor(255 * self.heartrate, 0, 255 * (1 - self.heartrate))
+    love.graphics.circle("fill", self.x, self.y, circle_size)
+  love.graphics.pop()
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.circle("line", self.x, self.y, circle_size )
+end
+
+function Target:update(dt, destroyer_heartrate)
+  
+  -- move
+  self.y = self.y + dt*self.SPEED
+  
+  -- destroy
+  local drate = math.abs(destroyer_heartrate - self.heartrate)
+  if drate < self.DAMAGE_THRESHOLD then
+    self.wobble = 1 - drate/self.DAMAGE_THRESHOLD
+  end
+  
+  -- destroy at end
+  if self.y > love.graphics.getHeight() + 100 then
+    self.purge = true
+  end
+end
 
 --[[------------------------------------------------------------
 GAME GAMESTATE
@@ -33,6 +79,10 @@ local state = GameState.new()
 
 function state:init()
   self.heartrate = 0.3
+  
+  self.timer = 0
+  
+  self.targets = {}
 end
 
 function state:enter()
@@ -57,21 +107,36 @@ function state:update(dt)
   if love.keyboard.isDown("up") then
     delta = delta + 1
   end
-
   if love.keyboard.isDown("down") then
     delta = delta - 1
   end
- 
   self.heartrate = useful.clamp(self.heartrate + delta*dt*0.3, 0, 1)
+  
+  -- create targets
+  self.timer = self.timer - dt
+  if self.timer < 0 then
+    self.timer = Target.INTERVAL 
+    table.insert(self.targets, Target())
+  end
+  
+  -- update targets
+  for i,v in ipairs(self.targets) do
+    v:update(dt, self.heartrate)
+  end
   
 end
 
 
 function state:draw()
+
   love.graphics.push()
     love.graphics.setColor(255 * self.heartrate, 0, 255 * (1 - self.heartrate))
     love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
   love.graphics.pop()
+  
+  for i,v in ipairs(self.targets) do
+    v:draw()
+  end
   
 end
 
